385886aff64ba9383c3aab42415a1faca869e21b
dimka3553 <37512400+dimka3553@users.noreply.github.com>
2023-05-26 08:04:03 +0200

    Create go_test.go

---
+++ go_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+  "testing"
+)
+
+func TestSuccess(t *testing.T) {
+  a := 2
+  b := 2
+  if a != b {
+    t.Error("Expected 2, got ", a)
+  }
+
+  c := a + b
+  if c != 4 {
+    t.Error("Expected 4, got ", c)
+  }  
+}
+
+func TestFail(t *testing.T) {
+  a := 2
+  b := 2
+  if a != b {
+    t.Error("Expected 2, got ", a)
+  }
+
+  c := a + b
+  if c != 5 {
+    t.Error("Expected 5, got ", c)
+  }
+}
